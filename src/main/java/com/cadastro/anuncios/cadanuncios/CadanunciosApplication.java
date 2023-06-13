15901a01082fc706ac35d778474e84f57d183547
luisbazzo <luis.o.c.bazzo@gmail.com>
2023-06-13 10:26:17 -0300

    1. Criar o projeto

---
+++ src/main/java/com/cadastro/anuncios/cadanuncios/CadanunciosApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro.anuncios.cadanuncios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadanunciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadanunciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cadastro/anuncios/cadanuncios/CadanunciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastro.anuncios.cadanuncios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadanunciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
