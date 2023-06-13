3b413cf864688c12e14fb37ed6d2ca8cc99bb153
luisbazzo <luis.o.c.bazzo@gmail.com>
2023-06-13 11:07:55 -0300

    4. Erros arrumados

--- src/main/java/com/cadastro/anuncios/cadanuncios/services/AdsServices.java
+++ src/main/java/com/cadastro/anuncios/cadanuncios/services/AdsServices.java
@@ -3,12 +3,14 @@ package com.cadastro.anuncios.cadanuncios.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cadastro.anuncios.cadanuncios.entities.Ads;
 import com.cadastro.anuncios.cadanuncios.repositories.AdsRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 
+@Service
 public class AdsServices {
     
     @Autowired
